0a9c48f195d190fb89a3054c96334214909ca932
alisia <alisia@OA-S141-22>
2026-06-09 17:42:23 -0400

    started coding gym

---
+++ Assets/Scripts(Summer)/ButtonColourChange.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class ButtonColourChange : MonoBehaviour
+{
+    public 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PressedColor = Random.ColorHSV();
+    }
+}
