6bf69bf834e06777bd8ed0b7d58c23809ee5d38d
Sian Ali <alisia@shernet.sheridancollege.ca>
2026-07-07 05:00:59 -0400

    made left and right keys rotate and transform position

--- Assets/Scripts(Summer)/Rotater.cs
+++ Assets/Scripts(Summer)/Rotater.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Rotater : MonoBehaviour
 {
+    public float moveSpeed;
+    public float rotationSpeed;
     public float speed = 1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -12,9 +15,33 @@ public class Rotater : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if ()
-        Vector3 newRotation = transform.eulerAngles;
-        newRotation.z += speed * Time.deltaTime;
-        transform.eulerAngles = newRotation;
+        
+        //right arrow key
+        bool isRightHeld = Keyboard.current.rightArrowKey.isPressed;
+        if (isRightHeld)
+        {
+            //rotate
+            transform.eulerAngles -= transform.forward * rotationSpeed * Time.deltaTime;
+
+            //moves
+            Vector3 newPosition = transform.position;
+            newPosition.x += speed;
+            transform.position = newPosition;
+            
+        }
+        //left arrow key
+        bool isLeftHeld = Keyboard.current.leftArrowKey.isPressed;
+        if (isLeftHeld)
+        {
+            //rotate
+            transform.eulerAngles += transform.forward * rotationSpeed * Time.deltaTime;
+
+            //move
+            Vector3 newPosition = transform.position;
+            newPosition.x += -speed;
+            transform.position = newPosition;
+        }
+        //rotation min and max
+        //transform min and max values (cap movement left screen)
     }
 }
